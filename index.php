d4f63377e83d10dd15e1f67029283fb717080ce3
George Oluwatobi Michael <horluwatowbeey@gmail.com>
2020-08-28 17:33:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -210,47 +210,24 @@
                 <div class="row portfolio-items">
                     <!--Portfolio Item-->
                     <div class="col-lg-4 col-md-6 item application">
-                        <div class="item-content">
+                        <div class="item-conten">
                            <a target="_blank" href="http://globalstreaming.herokuapp.com"> <img src="images/portfolio/img-6.jpg" alt=""></a>
-<!--                             <div class="item-overlay">
-                                <div class="icons">
-                                    <span class="icon link">
-                                        <a href="http://globalstreaming.herokuapp.com">
-                                            <i class="fa fa-search"></i>
-                                        </a>
-                                    </span>
-                                </div>
-                            </div> -->
+
                         </div>
                     </div>
                     <!--Portfolio Item-->
                     <div class="col-lg-4 col-md-6 item design">
-                        <div class="item-content">
+                        <div class="item-conten">
                            <a target="_blank" href="http://havimarheadstart.com"> <img src="images/portfolio/img-2.jpg" alt=""></a>
-<!--                             <div class="item-overlay">
-                                <div class="icons">
-                                    <span class="icon link">
-                                        <a href="http://havimarheadstart.com">
-                                            <i class="fa fa-search"></i>
-                                        </a>
-                                    </span>
-                                </div>
-                            </div> -->
+                           
                         </div>
                     </div>
                    
                     <!--Portfolio Item-->
                     <div class="col-lg-4 col-md-6 item development application">
-                        <div class="item-content">
+                        <div class="item-conten">
                            <a target="_blank" href="https://demusicnavigators.herokuapp.com/"> <img src="images/portfolio/img-5.jpg" alt=""></a>
-<!--                             <div class="item-overlay">
-                                <div class="icons">
-                                    <span class="icon link">
-                                        <a href="https://demusicnavigators.herokuapp.com/">
-                                            <i class="fa fa-search"></i>
-                                        </a>
-                                    </span>
-                                </div> -->
+                           
                             </div>
                         </div>
                     </div>
